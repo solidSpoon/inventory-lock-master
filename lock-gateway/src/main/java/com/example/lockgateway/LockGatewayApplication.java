9d56220cb461793f364c96e1a84b0f888b9099e4
cedarrival <cedarrival@foxmail.com>
2022-06-09 11:37:40 +0800

    init

---
+++ lock-gateway/src/main/java/com/example/lockgateway/LockGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockGatewayApplication.class, args);
+    }
+
+}
---
+++ lock-gateway/src/test/java/com/example/lockgateway/LockGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
